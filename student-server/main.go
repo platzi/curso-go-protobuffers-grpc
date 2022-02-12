e1367c4342c0b293fca57950429ace050bf173be
Nestor Escoto <nestor@smartifice.com>
2022-02-11 22:21:18 -0600

    Implementando SetStudent y GetStudent

--- student-server/main.go
+++ student-server/main.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"platzi.com/go/grpc/database"
+	"platzi.com/go/grpc/server"
+	"platzi.com/go/grpc/studentpb"
+)
+
+func main() {
+	list, err := net.Listen("tcp", ":5060")
+
+	if err != nil {
+		log.Fatalf("Error listening: %s", err.Error())
+	}
+
+	repo, err := database.NewPostgresRepository("postgres://postgres:postgres@localhost:54321/postgres?sslmode=disable")
+
+	server := server.NewStudentServer(repo)
+
+	if err != nil {
+		log.Fatalf("Error creating repository: %s", err.Error())
+	}
+
+	s := grpc.NewServer()
+	studentpb.RegisterStudentServiceServer(s, server)
+
+	reflection.Register(s)
+
+	if err := s.Serve(list); err != nil {
+		log.Fatalf("Error serving: %s", err.Error())
+	}
+}
